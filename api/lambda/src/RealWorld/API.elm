d7c6e234ff546b870ea8aae5670b29edbc2b9fce
Rupert Smith <rupert@thesett.com>
2019-06-25 13:30:39 +0100

    Implemented a dummy API to get started.

---
+++ api/lambda/src/RealWorld/API.elm
@@ -0,0 +1,55 @@
+port module RealWorld.API exposing (main)
+
+import Array exposing (Array)
+import Json.Decode as Decode exposing (Decoder)
+import Json.Encode as Encode
+import Model
+import Serverless
+import Serverless.Conn exposing (jsonBody, method, respond, route, textBody)
+import Serverless.Conn.Request exposing (Method(..), Request)
+import Url.Parser exposing ((</>), int, map, oneOf, s, top)
+
+
+port requestPort : Serverless.RequestPort msg
+
+
+port responsePort : Serverless.ResponsePort msg
+
+
+type Route
+    = Hello
+
+
+type alias Conn =
+    Serverless.Conn.Conn () () Route
+
+
+type alias Msg =
+    ()
+
+
+main : Serverless.Program () () Route Msg
+main =
+    Serverless.httpApi
+        { configDecoder = Serverless.noConfig
+        , initialModel = ()
+        , parseRoute =
+            oneOf
+                [ map Hello (s "hello")
+                ]
+                |> Url.Parser.parse
+        , endpoint = endpoint
+        , update = update
+        , requestPort = requestPort
+        , responsePort = responsePort
+        }
+
+
+endpoint : Conn -> ( Conn, Cmd Msg )
+endpoint conn =
+    ( conn, Cmd.none )
+
+
+update : Msg -> Conn -> ( Conn, Cmd Msg )
+update seed conn =
+    ( conn, Cmd.none )
