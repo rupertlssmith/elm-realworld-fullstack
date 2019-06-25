7d4376372f206b8d701175fdc247e9c77e992bdd
Rupert Smith <rupert@thesett.com>
2019-06-25 13:26:39 +0100

    Wiped out the spike API and updated the serverless config.

--- api/lambda/src/Revenue/api.js
+++
@@ -1,7 +0,0 @@
-const elmServerless = require('@the-sett/serverless-elm-bridge');
-
-const { Elm } = require('./API.elm');
-
-module.exports.handler = elmServerless.httpApi({
-  handler: Elm.Revenue.API
-});
